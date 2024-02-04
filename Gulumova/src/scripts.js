ad27cbc0a67b89cea2825063088f06bf2b87221f
mouthfulofchocolate <36697570+mouthfulofchocolate@users.noreply.github.com>
2024-02-04 14:29:19 +0330

    pretty much finished

--- Gulumova/src/scripts.js
+++ Gulumova/src/scripts.js
@@ -52,6 +52,107 @@ function button3(){
 
 }
 
+function button4(){
+    document.getElementById("button4").classList.add("active");
+    document.getElementById("button5").classList.remove("active");
+    document.getElementById("trc20").style.display="block";
+    document.getElementById("erc20").style.display="none";
+}
+
+function button5(){
+    document.getElementById("button4").classList.remove("active");
+    document.getElementById("button5").classList.add("active");
+    document.getElementById("trc20").style.display="none";
+    document.getElementById("erc20").style.display="block";
+}
+
+function copy1(){
+    let address = document.getElementById("trc20add").innerHTML;
+    navigator.clipboard.writeText(address)
+        .then(function() {
+            document.getElementById("copied1").classList.remove("hidden");
+            setTimeout(function(){
+                document.getElementById("copied1").classList.add("hidden");
+            },3000
+            )
+        })
+        .catch(function(err) {
+            console.error('Unable to copy text to clipboard', err);
+        });
+}
+
+function copy2(){
+    let address = document.getElementById("erc20add").innerHTML;
+    navigator.clipboard.writeText(address)
+        .then(function() {
+            document.getElementById("copied2").classList.remove("hidden");
+            setTimeout(function(){
+                document.getElementById("copied2").classList.add("hidden");
+            },3000
+            )
+        })
+        .catch(function(err) {
+            console.error('Unable to copy text to clipboard', err);
+        });
+}
+
+function checkTxid(){
+    let confirmed = false;
+    let amountAddressOk = false;
+    let txid = document.getElementById("txid").value;
+    document.getElementById('messageRelay').innerHTML = "";
+    fetch("https://api.blockcypher.com/v1/btc/main/txs/"+ txid +"?limit=50&includeHex=true#")
+        .then(
+            response => response.json()
+        )
+        .then(
+            data => {
+                if (data.confirmations > 1) {
+                    console.log("Transaction is confirmed");
+                    confirmed = true;
+                }
+                else {
+                    console.log("Transaction is not confirmed");
+                    document.getElementById('messageRelay').innerHTML = "transaction is not yet confirmed";
+                    return;
+                }
+
+                let outputs = data.outputs;
+
+                outputs.forEach( (output,index) => {
+
+                    let amountOk = false;
+                    let addressOk = false;
+
+                    if (output.value == targetAmount) {
+                        console.log("Amount is correct - index " + index);
+                        amountOk = true;
+                    }
+                
+                    if (output.addresses[0] == targetAdd) {
+                        console.log("Address is correct - index " + index);
+                        addressOk = true;
+                    }
+
+                    if (amountOk && addressOk) {
+                        amountAddressOk = true;
+                    }
+                })
+
+                if ( confirmed && amountAddressOk ) {
+                    document.getElementById("finishButton").removeAttribute("disabled");
+                    document.getElementById('messageRelay').innerHTML="all good! please proceed";
+                }
+                else {
+                    document.getElementById('messageRelay').innerHTML="invalid amount/destination";
+                }
+            })
+        .catch( error => {
+            console.log("Error: " + error);
+            document.getElementById('messageRelay').innerHTML = "invalid transaction";
+        })
+}
+
 // window.onload = function() {
 //     // Your code here, which will run after the page has fully loaded
 //     setTimeout(function() {
