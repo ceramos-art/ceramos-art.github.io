a951c8ad4c883beefc7a907f506b3b623938d159
mouthfulofchocolate <36697570+mouthfulofchocolate@users.noreply.github.com>
2024-04-06 17:45:57 +0330

    scroll bg

--- Firouzja/src/scripts.js
+++ Firouzja/src/scripts.js
@@ -5,3 +5,12 @@ const menuFunction = () => {
     else
         elm.classList.add("hidden");
 }
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var maxScroll = document.body.scrollHeight - window.innerHeight;
+    var percentageScrolled = (scrollPosition / maxScroll) * 100;
+    var backgroundColor = 'rgb(' + percentageScrolled + '%, ' + percentageScrolled + '%, ' + percentageScrolled + '%)';
+    document.body.style.backgroundColor = backgroundColor;
+    document.body.style.setProperty('--bg-color', backgroundColor);
+  });
