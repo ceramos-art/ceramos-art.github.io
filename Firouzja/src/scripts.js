d0f4a872ee97ce14135ae510728802c8df31e842
mouthfulofchocolate <36697570+mouthfulofchocolate@users.noreply.github.com>
2024-04-12 16:46:57 +0330

    adios

--- Firouzja/src/scripts.js
+++ Firouzja/src/scripts.js
@@ -6,11 +6,20 @@ const menuFunction = () => {
         elm.classList.add("hidden");
 }
 
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var maxScroll = document.body.scrollHeight - window.innerHeight;
-    var percentageScrolled = (scrollPosition / maxScroll) * 100;
-    var backgroundColor = 'rgb(' + percentageScrolled + '%, ' + percentageScrolled + '%, ' + percentageScrolled + '%)';
-    document.body.style.backgroundColor = backgroundColor;
-    document.body.style.setProperty('--bg-color', backgroundColor);
-  });
+// Function to change bg-color by scrolling -----------
+
+// window.addEventListener('scroll', function() {
+//     var scrollPosition = window.scrollY;
+//     var maxScroll = document.body.scrollHeight - window.innerHeight;
+//     var pcScrolled = (scrollPosition / maxScroll);
+    
+//     const color1 = [0,38,51];
+//     const color2 = [0,26,51];
+//     const color3 = [ Math.round(pcScrolled*color1[0]+(1-pcScrolled)*color2[0]) ,
+//         Math.round(pcScrolled*color1[1]+(1-pcScrolled)*color2[1]),
+//         Math.round(pcScrolled*color1[2]+(1-pcScrolled)*color2[2])];
+//     var backgroundColor = 'rgb('+color3[0]+','+color3[1]+','+color3[2]+')';
+
+//     document.body.style.backgroundColor = backgroundColor;
+//     document.body.style.setProperty('--bg-color', backgroundColor);
+//   });
